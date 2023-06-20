ae846a29c2053ea524a03212eb28618654eefc02
Dennis Tseng <36941125+Dennishi0925@users.noreply.github.com>
2023-06-20 11:18:04 +0800

    abc

--- import-data.R
+++ import-data.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(jsonlite)
 
+df_penguin <- fromJSON("https://assets.antv.antgroup.com/g2/penguins.json") %>% as_tibble()
 df_citizen <- fromJSON("data/citizen.json") %>% as_tibble()
-df_citizen ％
+df_penguin
+# df_citizen ％
