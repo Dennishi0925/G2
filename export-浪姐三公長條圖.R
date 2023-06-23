58e44626a57809765e1e578305cfe81cf6fbafaf
Dennis Tseng <36941125+Dennishi0925@users.noreply.github.com>
2023-06-23 16:14:45 +0800

    abc

---
+++ export-浪姐三公長條圖.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+df_sister_third <- read_rds("data/df_reply_third.rds")
+df_sister_agg <- df_sister_third %>% select(matches("請按照喜愛度排序")) %>%
+  mutate(id = row_number()) %>%
+  select(id, everything()) %>%
+  pivot_longer(cols = -1, names_to = "type", values_to = "value") %>%
+  mutate(type = str_extract(type, "\\[.*")) %>%
+  mutate(type = str_remove_all(type, "\\[|\\]")) %>%
+  count(type, value)
+
+df_sister_agg %>% write_csv("data/df_sister_agg.csv")
--- import-data.R
+++ import-data.R
@@ -3,5 +3,5 @@ library(jsonlite)
 
 df_penguin <- fromJSON("https://assets.antv.antgroup.com/g2/penguins.json") %>% as_tibble()
 df_citizen <- fromJSON("data/citizen.json") %>% as_tibble()
-df_penguin
-# df_citizen ％
+
+
